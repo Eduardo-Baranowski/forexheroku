92cd95d66f441557dbcda78f5faf1dc3db410090
Eduardo-Baranowski <eduardobaranowski@gmail.com>
2021-04-15 16:06:06 -0300

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         if (config('app.env') !== 'local') {
-            \URL::forceScheme('https');
+            \URL::forceScheme('http');
         }
     }
 
